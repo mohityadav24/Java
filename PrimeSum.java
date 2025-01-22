52f6f5c496ac872ff8ba975528a4cb44a368d5ba
mohityadav24 <112639626+mohityadav24@users.noreply.github.com>
2025-01-22 14:06:27 +0530

    Create PrimeSum.java

---
+++ PrimeSum.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+ class PrimeSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        
+
+        boolean found = false;
+        for (int i = 2; i <= n / 2; i++) {
+            if (isPrime(i) && isPrime(n - i)) {
+                System.out.println("Two prime numbers whose sum is " + n + " are: " + i + " and " + (n - i));
+                found = true;
+                break;
+            }
+        }
+        
+        if (!found) {
+            System.out.println("No prime numbers found whose sum is " + n);
+        }
+        
+        scanner.close();
+    }
+
+
+    public static boolean isPrime(int number) {
+	
+	int c=0;
+
+	for(int i =2;i<number;i++)
+	{
+		
+       
+          if(number % i ==0){
+			  return false;
+}
+
+		}
+	
+
+return true;
+
+ }
+ 
+ }
